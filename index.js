11c36e174613aad2db342f83c51918c85c31cb20
RANJITH K <smartranjithvip@gmail.com>
2023-02-05 00:12:55 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let randomNumberOne = Math.floor(Math.random()*6)+1;
+let randomNumberTwo = Math.floor(Math.random()*6)+1;
+
+
+let diceOneNumber = "/dice" + randomNumberOne + ".svg";
+let diceTwoNumber = "/dice" + randomNumberTwo + ".svg";
+
+
+let randomDiceOnePath ="./src" + diceOneNumber;
+
+let randomDicetwoPath ="./src" + diceTwoNumber;
+
+let dice1= document.querySelector(".dice1");
+
+dice1.setAttribute("src", randomDiceOnePath);
+
+let dice2= document.querySelector(".dice2");
+
+dice2.setAttribute("src", randomDicetwoPath);
+
+
+if(randomNumberOne>randomNumberTwo){
+ document.querySelector(".glow h3").innerText = "Winner : PLAYER 1";
+}
+else{ 
+    document.querySelector(".glow h3").innerText = "Winner : PLAYER 2";   
+}
